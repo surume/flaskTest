dfc359f7e2b40fc3e64ad0a20558627f78ec810b
Surume <dev.miyamoto.s@gmail.com>
2014-09-07 03:52:45 +0900

    テスト…

--- applications/Service/redisService.py
+++ applications/Service/redisService.py
@@ -12,6 +12,11 @@ redis = redis.from_url(redis_url)
 
 
 def setValueList(key, val):
+    if key is None:
+        return None
+    if val is None:
+        return None
+
     redis.rpush(key, str(val))
     result_list = redis.lrange(key, 0, -1)
     return [x.decode() for x in result_list]
--- testIndex.py
+++ testIndex.py
@@ -3,14 +3,12 @@
 
 # import flask
 import unittest
+from builtins import print
 
 import index
 import logging
+from applications.Service import redisService
 
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(message)s',
-                    )
 
 class TestIndex(unittest.TestCase):
 
@@ -23,8 +21,34 @@ class TestIndex(unittest.TestCase):
         logging.debug(response.data)
         assert response.data == b'FlaskTest!'
 
-    def test_get(self):
-        assert True
+
+
+
+class TestApiController(unittest.TestCase):
+
+    def setUp(self):
+        redisService.redis.flushall()
+
+    def test_null_keys(self):
+        key = None
+        val = "val"
+        assert None == redisService.setValueList(key, val)
+
+    def test_null_val(self):
+        key = "key"
+        val = None
+        assert None == redisService.setValueList(key, val)
+
+    def test_all_null(self):
+        key = None
+        val = None
+        assert None == redisService.setValueList(key, val)
+
+    def test_nomal(self):
+        redisService.redis.flushall()
+        key = "key"
+        val = "val"
+        assert ['val'] == redisService.setValueList(key, val)
 
 if __name__ == '__main__':
     unittest.main()
