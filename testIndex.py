05fdf9b98ca741d94bfba4778b4b0d99de606d3a
Surume <dev.miyamoto.s@gmail.com>
2014-09-07 03:10:35 +0900

    テスト構成悩んでる

---
+++ testIndex.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+# import flask
+import unittest
+
+import index
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    )
+
+class TestIndex(unittest.TestCase):
+
+    def setUp(self):
+        self.app = index.app.test_client()
+
+    def test_get(self):
+        response = self.app.get('/')
+        assert response.status_code == 200
+        logging.debug(response.data)
+        assert response.data == b'FlaskTest!'
+
+    def test_get(self):
+        assert True
+
+if __name__ == '__main__':
+    unittest.main()
